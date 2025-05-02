bff33f85c56a6d9a0a64c5721c68c6f37f1e96c2
VivianPeng689 <vivi689peng@gmail.com>
2025-05-02 11:30:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function draw() {
   push(); // 儲存當前畫布狀態
   translate(width, 0); // 將畫布的原點移到右上角
   scale(-1, 1); // 水平翻轉畫布
-  image(capture, (width - capture.width) / 2, (height - capture.height) / 2);
+  //image(capture, (width - capture.width) / 2, (height - capture.height) / 2);
   pop(); // 恢復畫布狀態
 
   // 顯示圖形內容在攝影機畫面的上方
